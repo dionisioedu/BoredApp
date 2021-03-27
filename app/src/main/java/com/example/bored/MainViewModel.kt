63a8927dc8f09370413141d179df98a7d41f23bd
cce_cfernandes <cce_cfernandes@uolinc.com>
2021-03-27 17:46:11 -0300

    Implements json parse

--- app/src/main/java/com/example/bored/MainViewModel.kt
+++ app/src/main/java/com/example/bored/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.bored
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bored.data.ActivityModel
 import com.example.bored.network.BoreApi
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,12 +18,12 @@ class MainViewModel() : ViewModel() {
 
     fun getNext() {
         BoreApi.retrofitService.getActivity().enqueue(
-            object : Callback<String> {
-                override fun onResponse(call: Call<String>, response: Response<String>) {
-                    _activity.value = response.body()
+            object : Callback<ActivityModel> {
+                override fun onResponse(call: Call<ActivityModel>, response: Response<ActivityModel>) {
+                    _activity.value = response.body()?.activity
                 }
 
-                override fun onFailure(call: Call<String>, t: Throwable) {
+                override fun onFailure(call: Call<ActivityModel>, t: Throwable) {
                     _activity.value = "Error : " + t.message
                 }
 
